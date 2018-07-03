d5d229e4d202a32cdd5d452781246e91449c23d5
volodindmitriy121 <volodindmitriy121@gmail.com>
2018-07-03 18:38:42 +0300

    Start part 5, update tests for login_reqired

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.contrib.auth.decorators import login_required
 from boards.models import Board, Post
 from .forms import NewTopicForm
 
@@ -14,7 +14,7 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
-
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
     user = User.objects.first()
