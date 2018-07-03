ad40f6ac391ac83312af1e98fbd0a0be0531dadd
volodindmitriy121 <volodindmitriy121@gmail.com>
2018-07-03 14:46:56 +0300

    Part 4

--- RoockLabTestBlog/urls.py
+++ RoockLabTestBlog/urls.py
@@ -1,13 +1,14 @@
 from boards import views
 from django.contrib import admin
 from django.conf.urls import url
-from django.urls import path
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
 ]
